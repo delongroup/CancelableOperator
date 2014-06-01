2df0846ca32f442eab4721eb29a81dd472cf648f
陈代龙 <delonchen@126.com>
2014-06-01 16:45:42 +0800

    +

--- OperatorDemo/Core/TaskManager.h
+++ OperatorDemo/Core/TaskManager.h
@@ -9,6 +9,16 @@
 #import <Foundation/Foundation.h>
 #import "TaskOperation.h"
 
+/**
+ *  任务管理
+ */
 @interface TaskManager : NSObject
+/**
+ *  执行任务
+ *
+ *  @param completion 任务完成时执行
+ *
+ *  @return 任务对象
+ */
 - (TaskOperation *)executeWithCompletion:(TaskCompletionBlock)completion;
 @end
